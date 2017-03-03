7edbbe2cf13823dc53a3dc8c900cdb9a8a437f4a
Scott Bessler <scottbessler@gmail.com>
2017-03-03 10:56:37 -0800

    init

---
+++ tslint.js
@@ -0,0 +1,50 @@
+export const rules = {
+    "rules": {
+        "no-var-keyword": true,
+        "prefer-const": true,
+        "no-conditional-assignment": true,
+        "no-shadowed-variable": true,
+        "no-switch-case-fall-through": true,
+        "no-string-throw": true,
+        "no-for-in-array": true,
+        "no-angle-bracket-type-assertion": true,
+        "no-trailing-whitespace": true,
+        "semicolon": [
+            true,
+            "always",
+            "ignore-interfaces"
+        ],
+        "array-type": [
+            true,
+            "array"
+        ],
+        "strict-boolean-expressions": [
+            true,
+            "allow-string",
+            "allow-undefined-union",
+            "allow-mix"
+        ],
+        "triple-equals": [
+            true,
+            "allow-undefined-check"
+        ],
+        "trailing-comma": [
+            true,
+            {
+                "multiline": "always",
+                "singleline": "never"
+            }
+        ],
+        "ban": [
+            true,
+            [
+                "it",
+                "only"
+            ],
+            [
+                "describe",
+                "only"
+            ]
+        ]
+    }
+}
