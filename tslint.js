44a6c6f86a703a48356e97b00937af2c1ab64688
Scott Bessler <scott@stridespark.com>
2017-05-23 21:44:31 -0700

    update for tslint5

--- tslint.js
+++ tslint.js
@@ -1,40 +1,52 @@
 module.exports = {
     "rules": {
         "no-var-keyword": true,
-        "prefer-const": true,
+        "prefer-const": { severity: 'warning' },
         "no-conditional-assignment": true,
-        "no-shadowed-variable": true,
-        "no-switch-case-fall-through": true,
+        "no-shadowed-variable": { severity: 'warning' },
+        "no-switch-case-fall-through": { severity: 'warning' },
         "no-string-throw": true,
         "no-angle-bracket-type-assertion": true,
-        "no-trailing-whitespace": true,
-        "no-unused-new": true,
+        "no-trailing-whitespace": { severity: 'warning' },
         "no-unused-expression": true,
-        "no-unused-variable": [
-            true,
-        ],
-        "semicolon": [
-            true,
-            "always",
-            "ignore-interfaces"
-        ],
-        "array-type": [
-            true,
-            "array"
-        ],
+        "await-promise": true,
+        "no-floating-promises": true,
+        "no-for-in-array": true,
+        "no-unbound-method": true,
+        "no-use-before-declare": true,
+        "strict-boolean-expressions": {
+            options: [
+                "allow-undefined-union",
+                "allow-string"
+            ]
+        },
+        "semicolon": {
+            severity: 'warning',
+            options: [
+                "always",
+                "ignore-interfaces"
+            ]
+        },
+        "array-type": {
+            severity: 'warning',
+            options: [
+                "array"
+            ]
+        },
         "triple-equals": [
             true,
             "allow-undefined-check"
         ],
-        "trailing-comma": [
-            true,
-            {
-                "multiline": "always",
-                "singleline": "never"
-            }
-        ],
+        "trailing-comma": {
+            severity: 'warning',
+            options: [
+                {
+                    "multiline": "always",
+                    "singleline": "never"
+                }
+            ]
+        },
         "ban": [
-            true,
             [
                 "it",
                 "only"
