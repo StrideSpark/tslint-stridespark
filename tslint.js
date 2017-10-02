79d7dd15d5fd9b93b4ec013aaf770c33d034402b
Scott Bessler <scott@getstride.com>
2017-10-02 15:42:12 -0700

    fix ban, and add mobx/lib/mobx blacklist

--- tslint.js
+++ tslint.js
@@ -1,49 +1,23 @@
 module.exports = {
-    "rules": {
-        "no-var-keyword": true,
-        "prefer-const": { severity: 'warning' },
-        "no-conditional-assignment": true,
-        "no-shadowed-variable": { severity: 'warning' },
-        "no-switch-case-fall-through": { severity: 'warning' },
-        "no-string-throw": true,
-        "no-angle-bracket-type-assertion": true,
-        "no-trailing-whitespace": { severity: 'warning' },
-        "no-default-export": { severity: 'warning' },
-        "semicolon": {
-            severity: 'warning',
-            options: [
-                "always",
-                "ignore-interfaces"
-            ]
-        },
-        "array-type": {
-            severity: 'warning',
-            options: [
-                "array"
-            ]
-        },
-        "triple-equals": [
-            true,
-            "allow-undefined-check"
-        ],
-        "trailing-comma": {
-            severity: 'warning',
-            options: [
-                {
-                    "multiline": "always",
-                    "singleline": "never"
-                }
-            ]
-        },
-        "ban": [
-            [
-                "it",
-                "only"
-            ],
-            [
-                "describe",
-                "only"
-            ]
-        ]
-    }
-}
+  rules: {
+    "no-var-keyword": true,
+    "prefer-const": { severity: "warning" },
+    "no-conditional-assignment": true,
+    "no-shadowed-variable": { severity: "warning" },
+    "no-switch-case-fall-through": { severity: "warning" },
+    "no-string-throw": true,
+    "no-angle-bracket-type-assertion": true,
+    "no-default-export": { severity: "warning" },
+    semicolon: {
+      severity: "warning",
+      options: ["always", "ignore-interfaces"]
+    },
+    "array-type": {
+      severity: "warning",
+      options: ["array"]
+    },
+    "triple-equals": [true, "allow-undefined-check"],
+    ban: [true, ["it", "only"], ["describe", "only"]],
+    "import-blacklist": [true, "mobx/lib/mobx"]
+  }
+};
